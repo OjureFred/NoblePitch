b4fb6f253879f79c3e542260a1e21853ac280b05
Fred Ojure <fredojure@hotmail.com>
2020-07-15 20:53:44 +0300

    Added user and role data objects

--- app/models.py
+++ app/models.py
@@ -1,14 +1,40 @@
-from . import def funcname(self, parameter_list):
-    raise NotImplementedError
+from . import db 
+
 class Pitch(db.Model):
     '''
-    Pitch class to define Pitch objects
-    ''''
-    __tablename__ = pitches
+    Pitch class to define Pitch objects and table
+    '''
+    __tablename__ = 'pitches'
     id = db.Column(db.Integer, primary_key=True)
-    title = db.Column(db.String, index=True)
-    description = db.Column(db.String, index=True)
-    category = db.Column(db.String, index = True)
+    title = db.Column(db.String(255), index=True)
+    description = db.Column(db.String(255), index=True)
+    category = db.Column(db.String(255), index = True)
     
     def __repr__(self):
         return f'Pitch {self.title}'
+
+class User(db.Model):
+    '''
+    Users class to define User objects and table
+    '''
+    __tablename__ = "users"
+
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(255))
+    role_id = db.Column(db.Integer, db.ForeignKey('roles.id'))
+
+    def __repr__(self):
+        return f'User {self.username}'
+
+class Role(db.Model):
+    '''
+    Class to define Role Object and table
+    '''
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(255))
+    users = db.relationship('User', backref = 'role', lazy = "dynamic")
+
+    def __repr__(self):
+        return f'Role {self.name}'
+
