552cddc1e6aab0ff388571efdcfd5737e3e28d76
Fred Ojure <fredojure@hotmail.com>
2020-07-16 16:55:21 +0300

    Finished coding login page

--- app/main/views.py
+++ app/main/views.py
@@ -2,7 +2,9 @@ from flask import render_template, request, redirect, url_for
 from flask_login import login_required
 from . import main
 #from .forms import PitchForm
-from ..models import Pitch
+from ..models import Pitch, User
+from .forms import RegistrationForm
+from .. import db
 
 @main.route('/')
 def index():
@@ -10,4 +12,16 @@ def index():
     Function to display index page
     '''
     message = "This is the trial page"
-    return render_template('index.html', message = message)
+    return render_template('index.html', message=message)
+
+@auth.route('/register',methods = ["GET","POST"])
+def register():
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        user = User(email = form.email.data, username = form.username.data,password = form.password.data)
+        db.session.add(user)
+        db.session.commit()
+        return redirect(url_for('auth.login'))
+        title = "New Account"
+    return render_template('auth/register.html',registration_form = form)
+    
