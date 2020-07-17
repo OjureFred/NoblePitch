db2a631f5e8024295df2eed2e364f1a0198887c1
Fred Ojure <fredojure@hotmail.com>
2020-07-17 05:06:44 +0300

    Corrected code to display pitches ion index page

--- app/main/views.py
+++ app/main/views.py
@@ -1,5 +1,5 @@
 from flask import render_template, request, redirect, url_for
-from flask_login import login_required
+from flask_login import login_required, current_user
 from . import main
 #from .forms import PitchForm
 from ..models import Pitch, User
@@ -10,12 +10,31 @@ def index():
     '''
     Function to display index page
     '''
-    pitch_list = Pitch.load_pitches()
+    pitch_list = Pitch.query.all()
 
     for pitch in pitch_list:
         print(pitch)
    
     return render_template('index.html', pitch_list=pitch_list)
+
+@main.route('/pitch/review/new/<int:id>', methods=['GET', 'POST'])
+@login_required
+def new_comment(id):
+    form = CommentForm()
+    pitch = get_pitch(id)
+    if form.validate_on_submit():
+        title = form.title.data
+        comment = form.review.data
+
+        # Updated comment instance
+        new_review = Review(movie_id=movie.id,movie_title=title,image_path=movie.poster,movie_review=review,user=current_user)
+
+        # save review method
+        new_review.save_review()
+        return redirect(url_for('.movie',id = movie.id ))
+
+    title = f'{movie.title} review'
+    return render_template('new_review.html',title = title, review_form=form, movie=movie)
 
 
 
--- app/models.py
+++ app/models.py
@@ -2,6 +2,7 @@ from . import db
 from . import login_manager
 from werkzeug.security import generate_password_hash, check_password_hash
 from flask_login import UserMixin
+from datetime import datetime
 
 class Pitch(db.Model):
     '''
@@ -13,6 +14,7 @@ class Pitch(db.Model):
     description = db.Column(db.String(2000), index=True)
     category = db.Column(db.String(255), index=True)
     
+    @classmethod
     def load_pitches():
         return Pitch.query.all()
     
@@ -30,9 +32,11 @@ class User(UserMixin, db.Model):
     email = db.Column(db.String(255), unique = True, index = True)
     role_id = db.Column(db.Integer, db.ForeignKey('roles.id'))
     pass_secure = db.Column(db.String(255))
+    comments = db.relationship('Comment', backref = 'user', lazy = "dynamic")
 
     def __repr__(self):
         return f'User {self.username}'
+
     @property
     def password(self):
         raise AttributeError('You cannot read the password attribute')
@@ -59,4 +63,26 @@ class Role(db.Model):
 
     def __repr__(self):
         return f'Role {self.name}'
+
+class Comment(db.Model):
+    '''
+    Funtion that defines Pitch comment
+    '''
+    __tablename__ = 'comments'
+
+    id = db.Column(db.Integer, primary_key=True)
+    pitch_id = db.Column(db.Integer)
+    pitch_title = db.Column(db.String)
+    pitch_comment = db.Column(db.String)
+    posted = db.Column(db.DateTime, default=datetime.utcnow)
+    user_id = db.Column(db.Integer, db.ForeignKey("users.id"))
+
+    def save_comment(self):
+        db.session.add(self)
+        db.session.commit()
+    
+    @classmethod
+    def get_comments(cls, id):
+        comment_list = Comment.query.filter_by(pith_id=id).all()
+        return comment_list
 
