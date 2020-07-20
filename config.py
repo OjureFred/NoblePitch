cf160d508ca0b79d36a08856598563517831008d
Fred Ojure <fredojure@hotmail.com>
2020-07-20 18:20:28 +0300

    Changed database name

--- config.py
+++ config.py
@@ -5,7 +5,7 @@ class Config:
     General configuration parent class
     '''
     SECRET_KEY = os.environ.get('SECRET_KEY')
-    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://postgres:admin@localhost/pitches'
+    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://postgres:admin@localhost/mpitches'
     UPLOADED_PHOTOS_DEST = 'app/static/photos'
 
     #Simple MDE configurations
